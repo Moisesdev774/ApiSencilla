d939939174ce4e4b3eb6eaf5ea624347ecd959c2
Moisés Bonilla <moisesbonilla2022@outlook.com>
2024-11-15 00:18:24 -0600

    Agregar archivos de proyecto.

---
+++ ApiSencilla.WebAPI/Data/ApiSencillaWebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiSencilla.WebAPI.Models;
+
+namespace ApiSencilla.WebAPI.Data
+{
+    public class ApiSencillaWebAPIContext : DbContext
+    {
+        public ApiSencillaWebAPIContext (DbContextOptions<ApiSencillaWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiSencilla.WebAPI.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ ApiSencilla.WebAPI/Documentos/dbo.Book.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Book]
+(
+
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(200) NOT NULL, 
+    [Author] NVARCHAR(100) NOT NULL, 
+    [Pages] INT NULL
+)
